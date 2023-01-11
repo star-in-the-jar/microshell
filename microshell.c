5e735d0de088c416a04996dca7105b063c0e11ff
Stanisław Jarocki <s.jarocki354@gmail.com>
2023-01-11 10:26:51 +0100

    Fix get command arguments.

--- microshell.c
+++ microshell.c
@@ -20,20 +20,21 @@ int main()
     /* declare variables */
     char *path = malloc(PATH_MAX_LENGTH);
     char *command = malloc(COMMAND_MAX_LENGTH);
-    char *commandArgv[MAX_ARGS];
-    // int commArgCount = 0;
+    char *commArgv[MAX_ARGS];
 
     /* welcome message */
-    showOff();
+    // showOff();
 
     while (1)
     {
         /* display current path */
         getCurrentWorkingDirectory(path, PATH_MAX_LENGTH);
-        printf("MICROSHELL [%s] $ ", path);
+        printf("MICRO [%s] $ ", path);
 
-        /* get input */
+        /* get user command */
         fgets(command, COMMAND_MAX_LENGTH, stdin);
+        size_t lastCharIndex = strcspn(command, "\n");
+        command[lastCharIndex] = 0;
 
         /* split line input into array of arguments */
         char *token;
@@ -42,116 +43,113 @@ int main()
         token = strtok(command, " ");
         while (token != NULL)
         {
-            commandArgv[i] = token;
+            /* check if argument length is lower than allowed */
+            size_t tokenLen = strlen(token);
+            if (tokenLen > MAX_ARG_LEN) {
+                printf("Error in (future) getArguments(): the given argument is too long");
+                return 1;
+            }
+
+            commArgv[i] = (char *)malloc(strlen(token) + 1);
+            strcpy(commArgv[i], token);
             token = strtok(NULL, " ");
             i++;
         }
 
+        /* point the end of array */
+        commArgv[i] = NULL;
+
         /* print all arguments */
         for (int j = 0; j < MAX_ARGS; j++)
         {
-            if (commandArgv[j] == NULL)
+            if (commArgv[j] == NULL)
             {
                 break;
             }
 
-            printf("Argument %d: %s\n", j + 1, commandArgv[j]);
-            commandArgv[j] = NULL;
+            printf("Argument %d: %s\n", j + 1, commArgv[j]);
+        }
+
+        /* free memory allocated for commArgv array */
+        for (int j = 0; j < MAX_ARGS; j++)
+        {
+            if (commArgv[j] == NULL)
+            {
+                break;
+            }
+            free(commArgv[j]);
+            commArgv[j] = NULL;
         }
     }
 
+    free(path);
+    free(command);
+
     return 0;
 }
+//     /* execute command based on first argument */
+//     if (strlen(commArgv[0]) == 0)
+//     {
+//         exit(0);
+//     }
+//     else if (strcmp(commArgv[0], "exit") == 0)
+//         exit(0);
+//     else if (strcmp(commArgv[0], "pwd") == 0)
+//     {
+//         printf("%s\n", path);
+//         getchar();
+//     }
+//     else if (strcmp(commArgv[0], "ls") == 0)
+//     {
+//         pid_t pid = fork();
 
-        // print all contents of commandArgv
-        // commArgCount = sizeof(commandArgv) / sizeof(commandArgv[0]);
-        // for (int i = 0; i < commArgCount; i++)
-        //     printf("Argument %d: %s\n", i + 1, commandArgv[i]);
+//         if (pid == -1)
+//         {
+//             printf("Error in ls: fork() returned -1\n");
+//         }
+//         else if (pid == 0)
+//         {
+//             execl("/bin/ls", commArgv[0], commArgv[1], NULL);
+//         }
+//         else
+//         {
+//             wait(NULL);
+//         }
+//     }
+//     else if (strcmp(commArgv[0], "cd") == 0)
+//     {
+//         /* if no arguments go to home directory */
+//         // if (ArgvNum == 0)
+//         // {
+//         //     printf("No arguments given, going to home directory\n");
+//         //     char *homePath = getenv("HOME");
+//         //     for (int i = 0; i < strlen(homePath); i++)
+//         //     {
+//         //         commArgv[1][i] = homePath[i];
+//         //     }
+//         // }
 
-    //     printf("hi");
-    //     exit(0);
+//         /* try to change directory */
+//         int result = chdir(commArgv[1]);
 
-    //     /*
-    //         int i = 0;
-    //         char inputCh;
+//         /* error handling */
+//         if (result == -1)
+//         {
+//             perror("Error in cd: wrong argument given");
+//         }
+//         else
+//         {
+//             printf("Changed directory to %s\n", commArgv[1]);
+//         }
+//     }
+//     else
+//     {
+//         printf("Unknown command: %s\n", commArgv[0]);
+//     }
 
-    //         while (inputCh != '\n')
-    //         {
-    //             inputCh = getchar();
-    //             command[i] = inputCh;
-    //             scanf("%c", &command);
-    //         }
-    //     */
-
-    //     /* temp print arguments */
-    //     /* for (int i = 0; i <= arg; i++)*/
-    //     /* {*/
-    //     /*     printf("%s\n", commandArgv[i]);*/
-    //     /* }*/
-
-    //     /* execute command based on first argument */
-    //     if (strlen(commandArgv[0]) == 0)
-    //     {
-    //         exit(0);
-    //     }
-    //     else if (strcmp(commandArgv[0], "exit") == 0)
-    //         exit(0);
-    //     else if (strcmp(commandArgv[0], "pwd") == 0)
-    //     {
-    //         printf("%s\n", path);
-    //         getchar();
-    //     }
-    //     else if (strcmp(commandArgv[0], "ls") == 0)
-    //     {
-    //         pid_t pid = fork();
-
-    //         if (pid == -1)
-    //         {
-    //             printf("Error in ls: fork() returned -1\n");
-    //         }
-    //         else if (pid == 0)
-    //         {
-    //             execl("/bin/ls", commandArgv[0], commandArgv[1], NULL);
-    //         }
-    //         else
-    //         {
-    //             wait(NULL);
-    //         }
-    //     }
-    //     else if (strcmp(commandArgv[0], "cd") == 0)
-    //     {
-    //         /* if no arguments go to home directory */
-    //         // if (ArgvNum == 0)
-    //         // {
-    //         //     printf("No arguments given, going to home directory\n");
-    //         //     char *homePath = getenv("HOME");
-    //         //     for (int i = 0; i < strlen(homePath); i++)
-    //         //     {
-    //         //         commandArgv[1][i] = homePath[i];
-    //         //     }
-    //         // }
-
-    //         /* try to change directory */
-    //         int result = chdir(commandArgv[1]);
-
-    //         /* error handling */
-    //         if (result == -1)
-    //         {
-    //             perror("Error in cd: wrong argument given");
-    //         }
-    //         else
-    //         {
-    //             printf("Changed directory to %s\n", commandArgv[1]);
-    //         }
-    //     }
-    //     else
-    //     {
-    //         printf("Unknown command: %s\n", commandArgv[0]);
-    //     }
-
-    //     free(path);
-    //     free(command);
-    // }
+//     free(path);
+//     free(command);
+// }
 //     return 0;
 // }
 
@@ -170,7 +168,7 @@ void getInput(char *command, size_t maxSize)
     fgets(command, maxSize, stdin);
 }
 
-void splitCommand(char *command, char **commandArgv)
+void splitCommand(char *command, char **commArgv)
 {
     char *token;
     int i = 0;
@@ -178,86 +176,87 @@ void splitCommand(char *command, char **commandArgv)
     token = strtok(command, " ");
     while (token != NULL)
     {
-        commandArgv[i++] = token;
+        commArgv[i++] = token;
         // printf("%s\n", token);
         token = strtok(NULL, " ");
     }
 }
-    // int commCh = 0, ArgvNum = 0, argCh = 0;
+// int commCh = 0, ArgvNum = 0, argCh = 0;
 
-    // while (commCh != '\n')
-    // {
-    //     /* terminate if user separated arguments with more than one space*/
-    //     if (command[commCh] == ' ' && command[commCh + 1] == ' ')
-    //     {
-    //         printf("Error: Too many spaces separating arguments\n");
-    //         exit(1);
-    //     }
+// while (commCh != '\n')
+// {
+//     /* terminate if user separated arguments with more than one space*/
+//     if (command[commCh] == ' ' && command[commCh + 1] == ' ')
+//     {
+//         printf("Error: Too many spaces separating arguments\n");
+//         exit(1);
+//     }
 
-    //     /* split input by space into array of arguments*/
-    //     if (command[commCh] == ' ')
-    //     {
-    //         commandArgv[ArgvNum][argCh] = '\0';
-    //         ArgvNum++;
-    //         argCh = 0;
-    //     }
-    //     else
-    //     {
-    //         commandArgv[ArgvNum][argCh] = command[commCh];
-    //         argCh++;
-    //     }
+//     /* split input by space into array of arguments*/
+//     if (command[commCh] == ' ')
+//     {
+//         commArgv[ArgvNum][argCh] = '\0';
+//         ArgvNum++;
+//         argCh = 0;
+//     }
+//     else
+//     {
+//         commArgv[ArgvNum][argCh] = command[commCh];
+//         argCh++;
+//     }
 
-    //     commCh++;
-    // }
+//     commCh++;
+// }
 
-void showOff() {
-    printf("\n");                                                                                                                                                                 
-    printf("\n");                                                                                                                                                                 
-    printf("\n");                                                                                                                                                                 
+void showOff()
+{
+    printf("\n");
+    printf("\n");
+    printf("\n");
     printf("    MMMMMMMM               MMMMMMMM iiii                                                                      hhhhhhh                              llllllllllllll ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M:::::::M             M:::::::Mi::::i                                                                     h:::::h                              l:::::ll:::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::::M           M::::::::M iiii                                                                      h:::::h                              l:::::ll:::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M:::::::::M         M:::::::::M                                                                           h:::::h                              l:::::ll:::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::::::M       M::::::::::iiiiiii    cccccccccccccccrrrrr   rrrrrrrrr     ooooooooooo      ssssssssss  h::::h hhhhh          eeeeeeeeeeee   l::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M:::::::::::M     M:::::::::::i:::::i  cc:::::::::::::::r::::rrr:::::::::r  oo:::::::::::oo  ss::::::::::s h::::hh:::::hhh     ee::::::::::::ee l::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M:::::::M::::M   M::::M:::::::Mi::::i c:::::::::::::::::r:::::::::::::::::ro:::::::::::::::ss:::::::::::::sh::::::::::::::hh  e::::::eeeee:::::el::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M M::::M M::::M M::::::Mi::::ic:::::::cccccc:::::rr::::::rrrrr::::::o:::::ooooo:::::s::::::ssss:::::h:::::::hhh::::::he::::::e     e:::::l::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M  M::::M::::M  M::::::Mi::::ic::::::c     cccccccr:::::r     r:::::o::::o     o::::os:::::s  ssssssh::::::h   h::::::e:::::::eeeee::::::l::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M   M:::::::M   M::::::Mi::::ic:::::c             r:::::r     rrrrrro::::o     o::::o  s::::::s     h:::::h     h:::::e:::::::::::::::::el::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M    M:::::M    M::::::Mi::::ic:::::c             r:::::r           o::::o     o::::o     s::::::s  h:::::h     h:::::e::::::eeeeeeeeeee l::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M     MMMMM     M::::::Mi::::ic::::::c     cccccccr:::::r           o::::o     o::::ssssss   s:::::sh:::::h     h:::::e:::::::e          l::::l l::::l ");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M               M::::::i::::::c:::::::cccccc:::::cr:::::r           o:::::ooooo:::::s:::::ssss::::::h:::::h     h:::::e::::::::e        l::::::l::::::l");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M               M::::::i::::::ic:::::::::::::::::cr:::::r           o:::::::::::::::s::::::::::::::sh:::::h     h:::::he::::::::eeeeeeeel::::::l::::::l");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    M::::::M               M::::::i::::::i cc:::::::::::::::cr:::::r            oo:::::::::::oo s:::::::::::ss h:::::h     h:::::h ee:::::::::::::el::::::l::::::l");
-    printf("\n");                                                                                                                                                                 
+    printf("\n");
     printf("    MMMMMMMM               MMMMMMMiiiiiiii   ccccccccccccccccrrrrrrr              ooooooooooo    sssssssssss   hhhhhhh     hhhhhhh   eeeeeeeeeeeeeelllllllllllllll");
-    printf("\n"); 
+    printf("\n");
 
-    printf("     _                 _              _     _                    _                      _    _ "); 
+    printf("     _                 _              _     _                    _                      _    _ ");
     printf("\n");
-    printf("    | |__  _   _   ___| |_ __ _ _ __ (_)___| | __ ___      __   (_) __ _ _ __ ___   ___| | _(_)"); 
+    printf("    | |__  _   _   ___| |_ __ _ _ __ (_)___| | __ ___      __   (_) __ _ _ __ ___   ___| | _(_)");
     printf("\n");
-    printf("    | '_ \\| | | | / __| __/ _` | '_ \\| / __| |/ _` \\ \\ /\\ / /   | |/ _` | '__/ _ \\ / __| |/ | |"); 
+    printf("    | '_ \\| | | | / __| __/ _` | '_ \\| / __| |/ _` \\ \\ /\\ / /   | |/ _` | '__/ _ \\ / __| |/ | |");
     printf("\n");
-    printf("    | |_) | |_| | \\__ | || (_| | | | | \\__ | | (_| |\\ V  V /    | | (_| | | | (_) | (__|   <| |"); 
+    printf("    | |_) | |_| | \\__ | || (_| | | | | \\__ | | (_| |\\ V  V /    | | (_| | | | (_) | (__|   <| |");
     printf("\n");
-    printf("    |_.__/ \\__, | |___/\\__\\__,_|_| |_|_|___|_|\\__,_| \\_/\\_/    _/ |\\__,_|_|  \\___/ \\___|_|\\_|_|"); 
+    printf("    |_.__/ \\__, | |___/\\__\\__,_|_| |_|_|___|_|\\__,_| \\_/\\_/    _/ |\\__,_|_|  \\___/ \\___|_|\\_|_|");
     printf("\n");
-    printf("           |___/                                              |__/                             "); 
+    printf("           |___/                                              |__/                             ");
     printf("\n");
     printf("\n");
     printf("\n");
